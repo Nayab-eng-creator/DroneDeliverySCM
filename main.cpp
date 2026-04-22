41845e6f68c0ac26f3e4543fa9ed93cc7174636c
Nayab-eng-creator <nayab.goher786@gmail.com>
2026-04-21 22:08:51 -0700

    Add files via upload
    Initial commit - added main.cpp

---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Drone Delivery System" << endl;
+    cout << "Tracking package and delivery status" << endl;
+    return 0;
+}
